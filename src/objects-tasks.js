7a9b6ec53bf0b041cf79a72ba2001be76b52a98b
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2024-03-03 21:20:31 +0300

    solved 8/21

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,14 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const output = {};
+  objects.forEach((a) => {
+    Object.entries(a).forEach(([key, value]) => {
+      output[key] = output[key] ? output[key] + value : value;
+    });
+  });
+  return output;
 }
 
 /**
